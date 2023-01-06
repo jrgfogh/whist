afcf353f190238d29320eb18fa0ac3659efd8bd7
Jørgen Fogh <jrgfogh@gmail.com>
2023-01-06 20:00:06 +0100

    Client: gameStateReducer() can receive the bidding winner.

--- Whist.Server/ClientApp/src/gameStateReducer.js
+++ Whist.Server/ClientApp/src/gameStateReducer.js
@@ -1,4 +1,6 @@
 export default function gameStateReducer(gameState, action)
 {
+    if (action.type === "bidding-winner")
+        return { state: "waiting" };
     return { state: "bidding", bids: [...gameState.bids, { bidder: action.chooser, bid: action.choice }] };
 }
--- Whist.Server/ClientApp/src/gameStateReducer.test.js
+++ Whist.Server/ClientApp/src/gameStateReducer.test.js
@@ -1,18 +1,30 @@
 import gameStateReducer from './gameStateReducer';
 
 describe("gameStateReducer", () => {
-    it.each([
-        ["Player A", "pass", []],
-        ["Player A", "9 common", []],
-        ["Player B", "pass", [{ bidder: "Player A", bid: "pass" }]],
-        ["Player C", "9 common", [{ bidder: "Player A", bid: "pass" }, { bidder: "Player B", bid: "9 common" }]]
-    ])('can receive the bid %p, after previous bids %p', (bidder, bid, previousBids) => {
+    describe("Bidding Round", () => {
+        it.each([
+            ["Player A", "pass", []],
+            ["Player A", "9 common", []],
+            ["Player B", "pass", [{ bidder: "Player A", bid: "pass" }]],
+            ["Player C", "9 common", [{ bidder: "Player A", bid: "pass" }, { bidder: "Player B", bid: "9 common" }]]
+        ])("Can receive the bid %p, after previous bids %p", (bidder, bid, previousBids) => {
 
-        const originalState = { state: "bidding", bids: previousBids };
-        const action = { type: "receive-choice", chooser: bidder, choice: bid };
+            const originalState = { state: "bidding", bids: previousBids };
+            const action = { type: "choice", chooser: bidder, choice: bid };
 
-        const expected = { state: "bidding", bids: [...previousBids, { bidder: bidder, bid: bid }] };
+            const expected = { state: "bidding", bids: [...previousBids, { bidder: bidder, bid: bid }] };
 
-        expect(gameStateReducer(originalState, action)).toEqual(expected);
-    })
+            expect(gameStateReducer(originalState, action)).toEqual(expected);
+        })
+
+        it("Can receive bidding winner", () => {
+            // NOTE(jrgfogh): We don't care that there are no previous bids. The rules are enforced on the server.
+            const originalState = { state: "bidding", bids: [] };
+            const action = { type: "bidding-winner", winner: "Player A", bid: "pass" };
+
+            const expected = { state: "waiting" };
+
+            expect(gameStateReducer(originalState, action)).toEqual(expected);
+        });
+    });
 });

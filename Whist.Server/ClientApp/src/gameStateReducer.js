9a24243e50b9b822167dc8641ff63e89acc279ba
Jørgen Fogh <jrgfogh@gmail.com>
2023-01-06 21:54:09 +0100

    Client: gameStateReducer() can receive dealt cards.

--- Whist.Server/ClientApp/src/gameStateReducer.js
+++ Whist.Server/ClientApp/src/gameStateReducer.js
@@ -1,5 +1,7 @@
 export default function gameStateReducer(gameState, action)
 {
+    if (action.type === "receive-cards")
+        return { state: "bidding", bids: [], cards: action.cards };
     if (action.type === "start-playing")
         return { state: "playing" };
     if (action.type === "bidding-winner")
--- Whist.Server/ClientApp/src/gameStateReducer.test.js
+++ Whist.Server/ClientApp/src/gameStateReducer.test.js
@@ -1,6 +1,20 @@
 import gameStateReducer from './gameStateReducer';
 
+// NOTE(jrgfogh): Many of the tests have invalid preconditions.
+// It doesn't matter, since the server handles the rules.
 describe("gameStateReducer", () => {
+    it.each([
+        [["Joker", "H1", "D5"]],
+        [["H1", "D5", "CJ"]]
+    ])("Can receive dealt cards", (cards) => {
+        const originalState = { state: "waiting" };
+        const action = { type: "receive-cards", cards: cards };
+
+        const expected = { state: "bidding", bids: [], cards: cards };
+
+        expect(gameStateReducer(originalState, action)).toEqual(expected);
+    });
+
     describe("Bidding Round", () => {
         it.each([
             ["Player A", "pass", []],
@@ -8,17 +22,15 @@ describe("gameStateReducer", () => {
             ["Player B", "pass", [{ bidder: "Player A", bid: "pass" }]],
             ["Player C", "9 common", [{ bidder: "Player A", bid: "pass" }, { bidder: "Player B", bid: "9 common" }]]
         ])("Can receive the bid %p, after previous bids %p", (bidder, bid, previousBids) => {
-
             const originalState = { state: "bidding", bids: previousBids };
             const action = { type: "choice", chooser: bidder, choice: bid };
 
             const expected = { state: "bidding", bids: [...previousBids, { bidder: bidder, bid: bid }] };
 
             expect(gameStateReducer(originalState, action)).toEqual(expected);
-        })
+        });
 
         it("Can receive bidding winner", () => {
-            // NOTE(jrgfogh): We don't care that there are no previous bids. The rules are enforced on the server.
             const originalState = { state: "bidding", bids: [] };
             const action = { type: "bidding-winner", winner: "Player A", bid: "pass" };
 
@@ -30,7 +42,6 @@ describe("gameStateReducer", () => {
 
     describe("Playing Round", () => {
         it("Can start playing", () => {
-            // NOTE(jrgfogh): We don't care that there are no bids. The rules are enforced on the server.
             const originalState = { state: "bidding", bids: [] };
             const action = { type: "start-playing" };
 

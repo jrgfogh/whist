4524c69cc12e97c7ff2149c023b2f108c57f3ac6
Jørgen Fogh <jrgfogh@gmail.com>
2023-01-07 16:27:15 +0100

    Client: Made ModalDialog() a proper react component.

--- Whist.Server/ClientApp/src/components/Home.js
+++ Whist.Server/ClientApp/src/components/Home.js
@@ -4,40 +4,40 @@ import { BidPicker } from "./BidPicker";
 import AcePicker from "./AcePicker";
 import { useCallback } from "react";
 
-function modalDialog(props)
+function ModalDialog({ dispatch, connection, gameState })
 {
   const choosebuddyAce = useCallback(async (card) => {
-      props.dispatch({ type: "user-chose-buddy-ace", choice: card });
-      await props.connection.invoke("SendChoice", `Buddy ace is ${card}`);
-    }, [props.dispatch, props.connection]);
+      dispatch({ type: "user-chose-buddy-ace", choice: card });
+      await connection.invoke("SendChoice", `Buddy ace is ${card}`);
+    }, [dispatch, connection]);
 
   const chooseTrump = useCallback(async (card) => {
       const trump = card[0];
-      props.dispatch({ type: "user-chose-trump", choice: trump });
-      await props.connection.invoke("SendChoice", `Trump is ${trump}`);
-    }, [props.dispatch, props.connection]);
+      dispatch({ type: "user-chose-trump", choice: trump });
+      await connection.invoke("SendChoice", `Trump is ${trump}`);
+    }, [dispatch, connection]);
 
-  if (props.gameState.state.endsWith("choosing-trump"))
+  if (gameState.state.endsWith("choosing-trump"))
     return (<div className="overlay">
         <AcePicker title="Please choose trump:" onChoice={chooseTrump} />
       </div>);
-  if (props.gameState.state.endsWith("choosing-buddy-ace"))
+  if (gameState.state.endsWith("choosing-buddy-ace"))
     return (<div className="overlay">
         <AcePicker title="Please choose buddy ace:" onChoice={choosebuddyAce} />
       </div>);
-  if (props.gameState.state.startsWith("bidding"))
-    return <BidPicker bids={props.gameState.bids || []} state={props.gameState.state}
-      dispatch={props.dispatch} connection={props.connection} />;
+  if (gameState.state.startsWith("bidding"))
+    return <BidPicker bids={gameState.bids || []} state={gameState.state}
+      dispatch={dispatch} connection={connection} />;
   return "";
 }
 
-export function Home(props) {
+export function Home({ dispatch, connection, gameState, cardsInHand, playCard }) {
   return (
       <div className="game-background">
         <Hand cards={[]} />
-        <Hand cards={props.cardsInHand} playing={props.gameState === "playing-choosing-card"}
-          playCard={props.playCard} />
-        {modalDialog(props)}
+        <Hand cards={cardsInHand} playing={gameState === "playing-choosing-card"}
+          playCard={playCard} />
+        <ModalDialog dispatch={dispatch} connection={connection} gameState={gameState} />
       </div>
     );
 }

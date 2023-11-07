6435b8416f05c9727cb7b48bd9d0aa746c9a8533
Jørgen Fogh <jrgfogh@gmail.com>
2023-11-07 21:23:07 +0100

    Fixed some CodeQL warnings. (In Microsoft's code!)

--- Whist.Server/ClientApp/src/setupProxy.js
+++ Whist.Server/ClientApp/src/setupProxy.js
@@ -1,8 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:56753';
+if (!env.ASPNETCORE_HTTPS_PORT) {
+    console.error("env.ASPNETCORE_HTTPS_PORT was not set!");
+}
+
+const target = `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`;
 
 const context = [
   "/WhistHub",
